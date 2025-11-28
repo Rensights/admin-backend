59a3bdf7adb3d29d0c1dbaa1b54a70d446c8ace1
ihakhverdiyev2019 <ihakhverdiyev2019@gmail.com>
2025-11-28 14:57:06 +0100

    Fix EntityManagerFactoryBuilder missing: Keep HibernateJpaAutoConfiguration to provide builder bean

--- src/src/main/java/com/rensights/admin/AdminApplication.java
+++ src/src/main/java/com/rensights/admin/AdminApplication.java
@@ -4,12 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(exclude = {
     DataSourceAutoConfiguration.class,
-    HibernateJpaAutoConfiguration.class,
     JpaRepositoriesAutoConfiguration.class
 })
 @ComponentScan(basePackages = "com.rensights.admin")
